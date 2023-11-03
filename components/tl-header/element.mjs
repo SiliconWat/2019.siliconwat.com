bf68aec83c6b84af523af05213e75b3478628552
Thon Ly <thonly@siliconwat.dev>
2023-11-03 11:08:40 -0700

    begin

--- components/tl-header/element.mjs
+++ components/tl-header/element.mjs
@@ -1,6 +1,27 @@
 import template from './template.mjs';
 
 class TlHeader extends HTMLElement {
+    #username = "thonly";
+
+    #public_menu = [
+        { title: "Menu: Home", page: "/" },
+        { title: "Porch & Front Yard", page: "/public/frontyard/" },
+        { title: "Main House", page: "/public/house/" },
+        { title: "Master Bedroom", page: "/public/master/", submenu: true },
+        { title: "Guest Bedroom", page: "/public/guest/", submenu: true },
+        { title: "Living Room", page: "/public/living/", submenu: true },
+        { title: "Kitchen", page: "/public/kitchen/", submenu: true },
+        { title: "Bathroom", page: "/public/bathroom/", submenu: true },
+        { title: "Back Yard", page: "/public/backyard/" }
+    ];
+
+    #private_menu = [
+        { title: "Menu: Home", page: "/" },
+        { title: "To Pack", page: "/private/pack/" },
+        { title: "To Lock Up", page: "/private/lockup/" },
+        { title: "Vacay", page: "/private/vacay/" }
+    ];
+
     constructor() {
         super();
         this.attachShadow({ mode: "open" });
@@ -8,7 +29,35 @@ class TlHeader extends HTMLElement {
     }
 
     connectedCallback() {
-        this.shadowRoot.querySelector(`option[value="${document.location.pathname}"]`).selected = true;
+        this.render();
+        this.shadowRoot.querySelectorAll('button').forEach(button => button.textContent = localStorage.getItem('credential') ? "Logout" : "Login");
+
+        const option = this.shadowRoot.querySelector(`option[value="${document.location.pathname}"]`);
+        if (option) option.selected = true
+        else this.shadowRoot.querySelector("option:first-child").selected = true;
+
+        const a = this.shadowRoot.querySelector(`a[href="${document.location.pathname}"]`);
+        if (a) a.style.color = "black";
+    }
+
+    render() {
+        const menu = localStorage.getItem('credential') ? this.#private_menu : this.#public_menu;
+        const ul = this.shadowRoot.querySelector('ul')
+        const select = this.shadowRoot.querySelector('select')
+        
+        menu.reverse().forEach(item => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.href = item.page; //a.setAttribute('href', item.page);
+            a.textContent = item.title;
+            li.appendChild(a)
+            ul.prepend(li)
+
+            const option = document.createElement('option');
+            option.value = item.page;
+            option.textContent = item.submenu ? "- " + item.title : item.title;
+            select.prepend(option);
+        });
     }
 
     page(select) {
@@ -16,7 +65,26 @@ class TlHeader extends HTMLElement {
     }
 
     menu(event) {
-        this.shadowRoot.querySelector('select').focus();
+        const credential = localStorage.getItem('credential');
+        if (credential) this.logout(event.target)
+        else this.login(event.target);
+    }
+
+    login(button, retry=false) {
+        button.disabled = true;
+        let username = window.prompt(retry ? "Incorrect username. Please try again:" : "Please enter your username:");
+        if (username) {
+            if (username.toLowerCase() === this.#username.toLowerCase()) {
+                localStorage.setItem('credential', username);
+                document.location = this.#private_menu[1].page;
+            } else this.login(button, true)
+        } else button.disabled = false;
+    }
+
+    logout(button) {
+        button.disabled = true;
+        localStorage.removeItem('credential');
+        document.location = this.#public_menu[0].page;
     }
 }
 
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import "/components/tl-header/element.mjs";
 import "/components/tl-footer/element.mjs";
 
-window.copyText = async event => {
-    await navigator.clipboard.writeText(event.target.previousElementSibling.value);
-    event.target.textContent = "Copied!";
-};
-
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-LZ0S4CE49V');
+gtag('config', 'G-3YC6KVFL7Y');
